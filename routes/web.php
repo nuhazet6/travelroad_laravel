2003ba0c9eb0acad83063f492821a5ac6cfd8bb4
Nuhazet Correa Torres <nuhazet6@gmail.com>
2024-01-18 16:37:29 +0000

    Añadido gitignore

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('travelroad');
+});
+
+Route::get('/wished', function () {
+  $wished = DB::select('select * from places where visited = false');
+
+  return view('wished', ['wished' => $wished]);
+});
+
+Route::get('/visited', function () {
+  $visited = DB::select('select * from places where visited = true');
+
+  return view('travelroad', ['visited' => $visited]);
 });
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => 'v10.3.1',
-        'version' => '10.3.1.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '433b3172dcd5eb5fac349d1bf17fdd90a59d9601',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -380,9 +380,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => 'v10.3.1',
-            'version' => '10.3.1.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '433b3172dcd5eb5fac349d1bf17fdd90a59d9601',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -757,8 +757,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0|3.0',
-                1 => '3.0.0',
+                0 => '3.0.0',
+                1 => '1.0|2.0|3.0',
             ),
         ),
         'psr/simple-cache' => array(
