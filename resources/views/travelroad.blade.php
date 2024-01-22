60e683f8e180d2dc3b121e073284080c0153ba97
Nuhazet Correa Torres <nuhazet6@gmail.com>
2024-01-22 19:02:57 +0000

    changed

--- resources/views/travelroad.blade.php
+++ resources/views/travelroad.blade.php
@@ -4,7 +4,7 @@
   </head>
 
   <body>
-    <h1>My Travel Bucket List</h1>
+    <h1>My Travel Bucket List - Changed</h1>
     <a href="/wished">Places I'd Like to Visit</a></br>
     <a href="/visited">Places I've Already Been To</a>
     <p>Powered By: Laravel</p>
